b4e7af1aa66d4037e6ef68bdde45ceabeaaf1c03
Jana3083 <jarkanum@gmail.com>
2026-02-22 19:06:08 -0300

    Especificação de teste, evidências e relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    //Configurações para geração dos relatorios (mochawesome)
+    // Configurações para geração dos relatórios (mochawesome)
     reporter: "mochawesome",
     reporterOptions: {
       reportDir: "cypress/reports",
@@ -12,5 +13,9 @@ module.exports = defineConfig({
       html: true,
       json: false,
     },
+    // Configurações de evidências
+    video: true, // grava vídeos automaticamente
+    screenshotsFolder: "cypress/screenshots", // pasta para screenshots
+    videosFolder: "cypress/videos", // pasta para vídeos
   },
 });
--- cypress/e2e/fluxo-completo.cy.js
+++ cypress/e2e/fluxo-completo.cy.js
@@ -1,38 +1,41 @@
 import 'cypress-xpath';
 
 // Especificação de testes 
-describe('Dashboard', () => {
+describe('fluxo-completo', () => {
 
   // Cenário de teste - sucesso
-  it('Acesso à Tela de Cadastro', () => {
+  it('Realizar o fluxo completo de cadastro e consulta', () => {
     // ARRANGE -> Acesso à página do sistema
-    cy.visit('https://cotiaws.github.io/controle-de-funcionarios/app/index.html');
-    cy.xpath('//*[@id="login-email"]').type('admin@admin.com');
-    cy.xpath('//*[@id="login-senha"]').type('123456');
-    cy.xpath('//*[@id="btn-login"]').click();
-    cy.xpath('//*[@id="btn-ir-cadastro"]').click();
+    cy.visit('https://cotiaws.github.io/controle-de-funcionarios/app/index.html'); // acesso à página do sistema
+    cy.xpath('//*[@id="login-email"]').type('admin@admin.com'); // preenchimento do campo de email para login
+    cy.xpath('//*[@id="login-senha"]').type('123456'); // preenchimento do campo de senha para login
+    cy.xpath('//*[@id="btn-login"]').click(); // ação para realizar o login no sistema
+    cy.xpath('//*[@id="btn-ir-cadastro"]').click(); // ação para acessar a tela de cadastro de funcionário
 
     // ACT -> Ações do teste
-    cy.xpath('//*[@id="cadastro-nome"]').type('Mariana Pereira');
-    cy.xpath('//*[@id="cadastro-cargo"]').type('Analista de Sistema');
-    cy.xpath('//*[@id="cadastro-salario"]').type('R$5000,00');
-    cy.xpath('//*[@id="btn-salvar-funcionario"]').click();
-    cy.xpath('//*[@id="btn-voltar-dashboard-cadastro"]').click();
-    cy.xpath('//*[@id="btn-ir-consulta"]').click();
-    cy.xpath('//*[@id="consulta-termo"]').type('Mariana Pereira');
-    cy.xpath('//*[@id="btn-pesquisar"]').click();
+    cy.xpath('//*[@id="cadastro-nome"]').type('Mariana Pereira'); // nome do funcionário a ser cadastrado
+    cy.xpath('//*[@id="cadastro-cargo"]').type('Analista de Sistema'); // cargo do funcionário a ser cadastrado
+    cy.xpath('//*[@id="cadastro-salario"]').type('R$5000,00'); // salário do funcionário a ser cadastrado
+    cy.xpath('//*[@id="btn-salvar-funcionario"]').click(); // ação para salvar o funcionário cadastrado
 
     // ASSERT -> Verificação do resultado
-    cy.xpath('//*[@id="col-nome-1"]').should('contain', 'Mariana Pereira');
+    cy.xpath('//*[@id="cadastro-sucesso"]').should('contain', 'Funcionário cadastrado com sucesso'); // validação da mensagem de sucesso do cadastro
 
-     // ACT -> Ações do teste
-    cy.xpath('//*[@id="btn-voltar-dashboard-consulta"]').click();
-    cy.xpath('//*[@id="btn-logout"]').click();
+    // ACT -> Ações do teste  
+    cy.xpath('//*[@id="btn-voltar-dashboard-cadastro"]').click(); // ação para voltar ao dashboard
+    cy.xpath('//*[@id="btn-ir-consulta"]').click(); // ação para acessar a tela de consulta
+    cy.xpath('//*[@id="consulta-termo"]').type('Mariana Pereira'); // termo de pesquisa para encontrar o funcionário cadastrado
+    cy.xpath('//*[@id="btn-pesquisar"]').click();// ação para realizar a pesquisa do funcionário cadastrado
 
+    // ASSERT -> Verificação do resultado
+    cy.xpath('//*[@id="col-nome-1"]').should('contain', 'Mariana Pereira'); // validação de que o funcionário cadastrado foi encontrado na consulta
 
+    // ACT -> Ações do teste
+    cy.xpath('//*[@id="btn-voltar-dashboard-consulta"]').click(); // ação para voltar ao dashboard
+    cy.xpath('//*[@id="btn-logout"]').click(); // ação para realizar logout do sistema
 
-
-
+    // ASSERT -> Verificação do resultado
+    cy.xpath('//*[@id="login-email"]').should('be.visible'); // validação de que a tela de login está visível após o logout do sistema    
 
     // EVIDÊNCIAS
     cy.screenshot('fluxo-completo', { overwrite: true });
