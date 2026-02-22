7462fdc104d358fe3d70b885daa87122ef5d123b
Jana3083 <jarkanum@gmail.com>
2026-02-22 17:50:24 -0300

    Criação dos testes de login de usuário com e sem sucesso

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,25 +1,40 @@
 import 'cypress-xpath';
 
-//Especificação de testes 
-describe('Login no sistema', () => {
+// Especificação de testes 
+describe('Login', () => {
 
-  //Cenário de teste 
+  // Cenário de teste - sucesso
   it('Deve fazer login com sucesso', () => {
-
-    //ARRANGE -> Acesso a página do sistema
+    // ARRANGE -> Acesso à página do sistema
     cy.visit('https://cotiaws.github.io/controle-de-funcionarios/app/index.html');
 
-
-    //ACT -> Etapa para realizar as ações do teste
+    // ACT -> Ações do teste
     cy.xpath('//*[@id="login-email"]').type('admin@admin.com');
     cy.xpath('//*[@id="login-senha"]').type('123456');
     cy.xpath('//*[@id="btn-login"]').click();
 
-    //ASSERT -> Etapa para verificar os resultados do teste 
+    // ASSERT -> Verificação do resultado
     cy.xpath('//*[@id="tela-dashboard"]/div[1]/h4').should('contain', 'Dashboard');
 
-    //EVIDÊNCIAS
-    cy.screenshot('Deve fazer login com sucesso', { overwrite : true });
-  })
+    // EVIDÊNCIAS
+    cy.screenshot('login-sucesso', { overwrite: true });
+  });
 
-}) 
+  // Cenário de teste - falha
+  it('Deve falhar ao tentar login', () => {
+    // ARRANGE -> Acesso à página do sistema
+    cy.visit('https://cotiaws.github.io/controle-de-funcionarios/app/index.html');
+
+    // ACT -> Ações do teste
+    cy.xpath('//*[@id="login-email"]').type('admin@admin.com');
+    cy.xpath('//*[@id="login-senha"]').type('123467'); // senha incorreta
+    cy.xpath('//*[@id="btn-login"]').click();
+
+    // ASSERT -> Verificação do resultado
+        cy.xpath('//*[@id="login-erro"]').should('contain', 'Login inválido');
+    
+    // EVIDÊNCIAS
+    cy.screenshot('login-erro', { overwrite: true });
+  });
+
+});
