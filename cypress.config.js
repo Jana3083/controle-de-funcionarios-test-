7afd190157e45548d1dc9ffea61a6675689a5e0a
Jana3083 <jarkanum@gmail.com>
2026-02-22 18:50:34 -0300

    Especificação de teste, evidências e relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    //Configurações para geração dos relatorios (mochawesome)
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: false,
     },
   },
 });
--- cypress/e2e/fluxo-completo.cy.js
+++ cypress/e2e/fluxo-completo.cy.js
@@ -1,5 +1,42 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import 'cypress-xpath';
+
+// Especificação de testes 
+describe('Dashboard', () => {
+
+  // Cenário de teste - sucesso
+  it('Acesso à Tela de Cadastro', () => {
+    // ARRANGE -> Acesso à página do sistema
+    cy.visit('https://cotiaws.github.io/controle-de-funcionarios/app/index.html');
+    cy.xpath('//*[@id="login-email"]').type('admin@admin.com');
+    cy.xpath('//*[@id="login-senha"]').type('123456');
+    cy.xpath('//*[@id="btn-login"]').click();
+    cy.xpath('//*[@id="btn-ir-cadastro"]').click();
+
+    // ACT -> Ações do teste
+    cy.xpath('//*[@id="cadastro-nome"]').type('Mariana Pereira');
+    cy.xpath('//*[@id="cadastro-cargo"]').type('Analista de Sistema');
+    cy.xpath('//*[@id="cadastro-salario"]').type('R$5000,00');
+    cy.xpath('//*[@id="btn-salvar-funcionario"]').click();
+    cy.xpath('//*[@id="btn-voltar-dashboard-cadastro"]').click();
+    cy.xpath('//*[@id="btn-ir-consulta"]').click();
+    cy.xpath('//*[@id="consulta-termo"]').type('Mariana Pereira');
+    cy.xpath('//*[@id="btn-pesquisar"]').click();
+
+    // ASSERT -> Verificação do resultado
+    cy.xpath('//*[@id="col-nome-1"]').should('contain', 'Mariana Pereira');
+
+     // ACT -> Ações do teste
+    cy.xpath('//*[@id="btn-voltar-dashboard-consulta"]').click();
+    cy.xpath('//*[@id="btn-logout"]').click();
+
+
+
+
+
+
+    // EVIDÊNCIAS
+    cy.screenshot('fluxo-completo', { overwrite: true });
+
+      });
+
+  });
